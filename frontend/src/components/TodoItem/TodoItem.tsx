002064fea77d3bd5c0afaaa6c472a198eda01163
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-03-07 14:34:53 +0000

    Style mobile view

--- frontend/src/components/TodoItem/TodoItem.tsx
+++ frontend/src/components/TodoItem/TodoItem.tsx
@@ -4,10 +4,12 @@ type TodoItemProps = {
 
 function TodoItem({ taskName }: TodoItemProps) {
   return (
-    <div className="flex h-[48px] w-[327px] items-center justify-center bg-white text-[12px]/[100%]">
-      <div className="flex h-[20px] w-[286.79] items-center justify-center">
-        <input type="checkbox" name="" id="" />
-        <p>{taskName}</p>
+    <div className="flex h-[48px] min-w-[327px] items-center px-[20.11px] py-[16px] text-[12px]/[100%] first:rounded-t-[5px]">
+      <div className="flex min-w-[286.79px] justify-between">
+        <div className="flex items-center gap-[16px]">
+          <input type="checkbox" name="" id="" />
+          <p>{taskName}</p>
+        </div>
         <button>
           <img src="/icon-cross.svg" alt="" />
         </button>
