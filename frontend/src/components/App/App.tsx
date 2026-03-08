58935654f29b4a8ecd631b481febb25f061d01bd
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-03-08 12:36:46 +0000

    Add more styles to mobile view

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -12,13 +12,13 @@ function App() {
             alt="MDN"
           />
         </picture>
-        <div className="fixed top-[48px] mx-[24px] mt-[48px] flex min-w-[327px] flex-col gap-[40px]">
-          <div className="flex justify-between">
-            <p className="text-white ">TODO</p>
+        <div className="fixed top-[48px] flex min-w-[327px] flex-col gap-[40px]">
+          <div className="flex items-center justify-between">
+            <p className="text-[30px] tracking-[0.4em] text-white">TODO</p>
             <img src="/icon-moon.svg" alt="" />
           </div>
           <div className="flex flex-col gap-[16px]">
-            <div className="flex h-[48px] min-w-[327px] items-center rounded-[5px] bg-white px-[20.11px] py-[16px] text-[12px]/[100%]">
+            <div className="flex h-[48px] min-w-[327px] items-center rounded-[5px] bg-white px-[20.11px] text-[12px]/[100%]">
               <input
                 type="text"
                 placeholder="Create a new todo..."
@@ -28,24 +28,30 @@ function App() {
             </div>
             <div className="divide-y divide-[#C8CBE7] rounded-[5px] bg-white">
               <TodoItem taskName="Complete online JavaScript course" />
-              <TodoItem taskName="jog around the park 3x" />
+              <TodoItem taskName="Jog around the park 3x" />
               <TodoItem taskName="10 minutes meditation" />
               <TodoItem taskName="Read for 1 hour" />
               <TodoItem taskName="Pick up groceries" />
               <TodoItem taskName="Complete Todo App on Frontend Mentor" />
               <div className="flex h-[48px] min-w-[327px] items-center rounded-b-[5px] bg-white px-[20.11px] py-[16px] text-[12px]/[100%]">
                 <div className="flex min-w-[286.79px] justify-between text-[#9495A5]">
-                  <div className="text-[12px]/[100% tracking-[-0.25px]">
+                  <div className="text-[12px]/[100%] tracking-[-0.25px]">
                     5 items left
                   </div>
                   <div>Clear Completed</div>
                 </div>
               </div>
             </div>
-            <div className="flex h-[48px] min-w-[327px] items-center justify-center gap-[10px] rounded-[5px] bg-white">
-              <button>All</button>
-              <button>Active</button>
-              <button>Completed</button>
+            <div className="flex h-[48px] min-w-[327px] items-center justify-center gap-[10px] rounded-[5px] bg-white font-josefin-sans text-[14px]/[100%] tracking-[-0.25px] text-[#9495A5]">
+              <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
+                All
+              </button>
+              <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
+                Active
+              </button>
+              <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
+                Completed
+              </button>
             </div>
           </div>
           <p className="self-center text-[#9495A5]">
--- frontend/src/components/TodoItem/TodoItem.tsx
+++ frontend/src/components/TodoItem/TodoItem.tsx
@@ -4,11 +4,13 @@ type TodoItemProps = {
 
 function TodoItem({ taskName }: TodoItemProps) {
   return (
-    <div className="flex h-[48px] min-w-[327px] items-center px-[20.11px] py-[16px] text-[12px]/[100%] first:rounded-t-[5px]">
+    <div className="flex h-[48px] min-w-[327px] items-center px-[20.11px] py-[16px] first:rounded-t-[5px]">
       <div className="flex min-w-[286.79px] justify-between">
         <div className="flex items-center gap-[16px]">
           <input type="checkbox" name="" id="" />
-          <p>{taskName}</p>
+          <p className="font-josefin-sans text-[12px]/[100%] tracking-[-0.25px] text-[#494C6B]">
+            {taskName}
+          </p>
         </div>
         <button>
           <img src="/icon-cross.svg" alt="" />
