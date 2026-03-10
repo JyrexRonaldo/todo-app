5117fdd5d84dd4a30cb97f6a5fe6721e8f8f13ee
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-03-10 11:03:41 +0000

    Reposition elements using transparent container

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -3,8 +3,8 @@ import TodoItem from "../TodoItem/TodoItem";
 function App() {
   return (
     <>
-      <div className="flex h-screen flex-col items-center bg-[#FAFAFA]">
-        <picture className="self-stretch">
+      <div className="relative flex h-screen flex-col items-center bg-[#FAFAFA]">
+        <picture className="w-full">
           <source srcSet="/bg-desktop-light.jpg" media="(width >= 640px)" />
           <img
             className="h-auto w-full object-cover"
@@ -12,51 +12,53 @@ function App() {
             alt="MDN"
           />
         </picture>
-        <div className="fixed top-[48px] flex min-w-[327px] flex-col gap-[40px]">
-          <div className="flex items-center justify-between">
-            <p className="text-[30px] tracking-[0.4em] text-white">TODO</p>
-            <img src="/icon-moon.svg" alt="" />
-          </div>
-          <div className="flex flex-col gap-[16px]">
-            <div className="flex h-[48px] min-w-[327px] items-center rounded-[5px] bg-white px-[20.11px] text-[12px]/[100%]">
-              <input
-                type="text"
-                placeholder="Create a new todo..."
-                name=""
-                id=""
-              />
+        <div className="absolute h-screen w-full bg-red-600/0">
+          <div className="top-[48px] mx-[20px] flex flex-col gap-[40px] ">
+            <div className="flex items-center justify-between">
+              <p className="text-[30px] tracking-[0.4em] text-white">TODO</p>
+              <img src="/icon-moon.svg" alt="" />
             </div>
-            <div className="divide-y divide-[#C8CBE7] rounded-[5px] bg-white">
-              <TodoItem taskName="Complete online JavaScript course" />
-              <TodoItem taskName="Jog around the park 3x" />
-              <TodoItem taskName="10 minutes meditation" />
-              <TodoItem taskName="Read for 1 hour" />
-              <TodoItem taskName="Pick up groceries" />
-              <TodoItem taskName="Complete Todo App on Frontend Mentor" />
-              <div className="flex h-[48px] min-w-[327px] items-center rounded-b-[5px] bg-white px-[20.11px] py-[16px] text-[12px]/[100%]">
-                <div className="flex min-w-[286.79px] justify-between text-[#9495A5]">
-                  <div className="text-[12px]/[100%] tracking-[-0.25px]">
-                    5 items left
+            <div className="flex flex-col gap-[16px]">
+              <div className="flex h-[48px] items-center rounded-[5px] bg-white px-[20.11px] text-[12px]/[100%]">
+                <input
+                  type="text"
+                  placeholder="Create a new todo..."
+                  name=""
+                  id=""
+                />
+              </div>
+              <div className="divide-y divide-[#C8CBE7] rounded-[5px] bg-white">
+                <TodoItem taskName="Complete online JavaScript course" />
+                <TodoItem taskName="Jog around the park 3x" />
+                <TodoItem taskName="10 minutes meditation" />
+                <TodoItem taskName="Read for 1 hour" />
+                <TodoItem taskName="Pick up groceries" />
+                <TodoItem taskName="Complete Todo App on Frontend Mentor" />
+                <div className="flex h-[48px] items-center rounded-b-[5px] bg-white px-[20.11px] py-[16px] text-[12px]/[100%]">
+                  <div className="flex justify-between text-[#9495A5]">
+                    <div className="text-[12px]/[100%] tracking-[-0.25px]">
+                      5 items left
+                    </div>
+                    <div>Clear Completed</div>
                   </div>
-                  <div>Clear Completed</div>
                 </div>
               </div>
+              <div className="flex h-[48px] items-center justify-center gap-[10px] rounded-[5px] bg-white font-josefin-sans text-[14px]/[100%] tracking-[-0.25px] text-[#9495A5]">
+                <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
+                  All
+                </button>
+                <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
+                  Active
+                </button>
+                <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
+                  Completed
+                </button>
+              </div>
             </div>
-            <div className="flex h-[48px] min-w-[327px] items-center justify-center gap-[10px] rounded-[5px] bg-white font-josefin-sans text-[14px]/[100%] tracking-[-0.25px] text-[#9495A5]">
-              <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
-                All
-              </button>
-              <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
-                Active
-              </button>
-              <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
-                Completed
-              </button>
-            </div>
+            <p className="self-center text-[#9495A5]">
+              Drag and drop to reorder list
+            </p>
           </div>
-          <p className="self-center text-[#9495A5]">
-            Drag and drop to reorder list
-          </p>
         </div>
       </div>
     </>
--- frontend/src/components/TodoItem/TodoItem.tsx
+++ frontend/src/components/TodoItem/TodoItem.tsx
@@ -4,8 +4,8 @@ type TodoItemProps = {
 
 function TodoItem({ taskName }: TodoItemProps) {
   return (
-    <div className="flex h-[48px] min-w-[327px] items-center px-[20.11px] py-[16px] first:rounded-t-[5px]">
-      <div className="flex min-w-[286.79px] justify-between">
+    <div className="flex h-[48px] items-center px-[20.11px] py-[16px] first:rounded-t-[5px]">
+      <div className="flex justify-between">
         <div className="flex items-center gap-[16px]">
           <input
             className="relative h-[20px] w-[20px] appearance-none rounded-[50%] border border-[#979797] align-[-2px] text-white before:invisible before:absolute before:top-[-1px] before:right-[-1px] before:block before:size-[20px] before:rounded-full before:bg-linear-135 before:from-[hsl(192,100%,67%)] before:to-[hsl(280,87%,65%)] after:invisible after:absolute after:top-[-1px] after:left-[3.5px] after:block after:text-[13px] after:content-['✓'] checked:before:visible checked:after:visible"
