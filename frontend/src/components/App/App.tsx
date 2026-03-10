d40aeefbfca01b49295f05645f642a2e99d42bb7
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-03-10 12:45:56 +0000

    Fix could not find decalration module error
    changing the file format to tsx fixes could not
    find declaration error

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -49,17 +49,6 @@ function App() {
                   </div>
                 </div>
               </div>
-              {/* <div className="flex h-[48px] items-center justify-center gap-[10px] rounded-[5px] bg-white font-josefin-sans text-[14px]/[100%] tracking-[-0.25px] text-[#9495A5]">
-                <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
-                  All
-                </button>
-                <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
-                  Active
-                </button>
-                <button className="hover:text-[#3A7CFD] active:text-[#3A7CFD]">
-                  Completed
-                </button>
-              </div> */}
               <div className="sm:hidden">
                 <TabBar />
               </div>
