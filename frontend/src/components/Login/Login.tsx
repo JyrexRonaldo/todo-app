1eff2730464a97c340a3742196625a18c46655cb
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-03-26 11:50:23 +0000

    Add dark mode for login and signup components

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 function Login() {
   return (
-    <div className="flex flex-col gap-3 rounded-[5px] bg-white p-7 font-josefin-sans drop-shadow-2xl">
+    <div className="flex flex-col gap-3 rounded-[5px] bg-white p-7 font-josefin-sans drop-shadow-2xl dark:bg-[#25273D] dark:text-[#E3E4F1]">
       <div className="flex flex-col gap-2">
         <label htmlFor="email" className="text-[14px]/[100%]">
           Email Address
@@ -9,8 +9,8 @@ function Login() {
           type="email"
           name="email"
           placeholder="Enter your email"
-          className="rounded-[5px] border border-[#C8CBE7] p-[8px]"
-          id=""
+          className="rounded-[5px] border border-[#C8CBE7] p-[8px] dark:border-[#393A4B] dark:bg-[#171823]"
+          id="email"
         />
       </div>
       <div className="flex flex-col gap-2">
@@ -21,15 +21,15 @@ function Login() {
           type="password"
           name="password"
           placeholder="Enter your password"
-          className="rounded-[5px] border border-[#C8CBE7] p-[8px]"
-          id=""
+          className="rounded-[5px] border border-[#C8CBE7] p-[8px] dark:border-[#393A4B] dark:bg-[#171823]"
+          id="password"
         />
       </div>
-      <button className="cursor-pointer rounded-[5px] bg-[#3A7CFD] p-[10px] text-white transition duration-300 hover:bg-blue-600">
+      <button className="cursor-pointer rounded-[5px] bg-[#3A7CFD] p-[10px] text-white transition duration-300 hover:bg-blue-600 active:scale-105 dark:bg-blue-600 dark:hover:bg-[#3A7CFD]">
         Login
       </button>
       <a
-        className="cursor-pointer text-center text-[14px]/[100%] text-[#3A7CFD]"
+        className="cursor-pointer text-center text-[14px]/[100%] text-[#3A7CFD] transition duration-300 hover:text-blue-400 hover:underline dark:text-[#E3E4F1]"
         href=""
       >
         Don't have an account? Create one
