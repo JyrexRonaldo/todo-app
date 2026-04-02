acdb566af0644bd3f3f22a053485a30253c59f64
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-04-02 17:10:24 +0000

    Add rate limiting for request to backend

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,20 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const rateLimit = require("express-rate-limit");
 const todoRouter = require("./routes/todoRouter");
 const authRouter = require("./routes/authRouter");
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
