c12f8e0cb6834471b6d5f455d90a6aef5be5ed46
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2026-04-09 23:11:49 +0000

    Prevent create todo text field from causing render

--- frontend/src/components/TodoList/TodoList.tsx
+++ frontend/src/components/TodoList/TodoList.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import TodoItem from "../TodoItem/TodoItem";
 import { DragDropProvider } from "@dnd-kit/react";
 import { isSortable } from "@dnd-kit/react/sortable";
+import { useForm, type SubmitHandler, type FieldErrors } from "react-hook-form";
 
 type AllTodos = {
   id: number;
@@ -11,8 +12,16 @@ type AllTodos = {
   position: number;
 }[];
 
+type TodoListType = {
+  todoText: string;
+  completeStatus: boolean;
+  todoId: number;
+};
+
 function TodoList() {
-  const [todoText, setTodoText] = useState("");
+  const { register, handleSubmit, resetField } = useForm<TodoListType>();
+
+  // const [todoText, setTodoText] = useState("");
   const [allTodos, setAllTodos] = useState<AllTodos>([]);
   const [list, setList] = useState("all");
 
@@ -40,41 +49,82 @@ function TodoList() {
     fetchData();
   }, []);
 
-  function handleTodoInput(
-    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
-  ) {
-    setTodoText(e.target.value);
+  // function handleTodoInput(
+  //   e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,
+  // ) {
+  //   setTodoText(e.target.value);
+  // }
+
+  const onSubmit: SubmitHandler<TodoListType> = (formData) => {
+    uploadTodo(formData.todoText);
+    resetField("todoText");
+  };
+
+  const onError = (error: FieldErrors) => {
+    console.log(error);
+  };
+
+  async function uploadTodo(todoText: string) {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_HOME_DOMAIN}/api/todos`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("userToken")}`,
+          },
+          body: JSON.stringify({
+            userId: `${localStorage.getItem("userId")}`,
+            text: `${todoText}`,
+            position: allTodos.length,
+          }),
+        },
+      );
+
+      const data: AllTodos = await response.json();
+      setAllTodos([...allTodos, ...data]);
+    } catch (error) {
+      console.log(error);
+    }
+    // setTodoText("");
   }
 
   async function handleAddTodo(e: React.KeyboardEvent<HTMLInputElement>) {
+    // e.preventDefault()
+    console.log("Hi 5");
+    console.log(e.key);
     if (e.key === "Enter") {
-      if (todoText.trim() === "") {
-        return;
-      }
-      try {
-        const response = await fetch(
-          `${import.meta.env.VITE_HOME_DOMAIN}/api/todos`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `${localStorage.getItem("userToken")}`,
-            },
-            body: JSON.stringify({
-              userId: `${localStorage.getItem("userId")}`,
-              text: `${todoText}`,
-              position: allTodos.length,
-            }),
-          },
-        );
-
-        const data: AllTodos = await response.json();
-        setAllTodos([...allTodos, ...data]);
-      } catch (error) {
-        console.log(error);
-      }
-      setTodoText("");
+      handleSubmit(onSubmit, onError)();
     }
+    // if (e.key === "Enter") {
+    // if (todoText.trim() === "") {
+    // return;
+    // }
+    // try {
+    //   const response = await fetch(
+    //     `${import.meta.env.VITE_HOME_DOMAIN}/api/todos`,
+    //     {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Authorization: `${localStorage.getItem("userToken")}`,
+    //       },
+    //       body: JSON.stringify({
+    //         userId: `${localStorage.getItem("userId")}`,
+    //         text: `${todoText}`,
+    //         position: allTodos.length,
+    //       }),
+    //     },
+    //   );
+
+    //   const data: AllTodos = await response.json();
+    //   setAllTodos([...allTodos, ...data]);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    // setTodoText("");
+    // }
   }
 
   async function handleDeleteTodo(todoId: number) {
@@ -169,13 +219,13 @@ function TodoList() {
       const updatedItemIndex = allTodos.findIndex(
         (todo) => todo.id === data.todo.id,
       );
-       allTodos.splice(updatedItemIndex, 1, data.todo)
+      allTodos.splice(updatedItemIndex, 1, data.todo);
       // console.log(updatedItemIndex);
       // console.log(data.todo);
       // console.log(allTodos);
       // setAllTodos([...allTodos, data.todo]);
       // console.log(updatedTodoList)
-      console.log(allTodos)
+      console.log(allTodos);
       setAllTodos([...allTodos]);
     } catch (error) {
       console.log(error);
@@ -241,7 +291,7 @@ function TodoList() {
 
   return (
     <>
-      <DragDropProvider
+      {/* <DragDropProvider
         onDragEnd={async (event) => {
           if (event.canceled) return;
 
@@ -260,22 +310,42 @@ function TodoList() {
             }
           }
         }}
-      >
-        <div className="flex flex-col gap-[16px] drop-shadow-2xl">
-          <div className="flex h-[48px] items-center rounded-[5px] bg-white px-[20.11px] text-[12px]/[100%] dark:bg-[#25273D] dark:text-[#9495A5]">
-            <div className="mr-[16px] h-[20px] w-[20px] rounded-[50%] border border-[#979797] dark:border-[#393A4B]"></div>
-            <input
-              type="text"
-              placeholder="Create a new todo..."
-              name=""
-              id=""
-              onKeyUp={handleAddTodo}
-              onChange={handleTodoInput}
-              value={todoText}
-              className="w-full font-josefin-sans text-[12px]/[100%] outline-none sm:text-[18px]/[100%]"
-            />
-          </div>
-          <div className="divide-y divide-[#C8CBE7] rounded-[5px] bg-white dark:divide-[#393A4B] dark:bg-[#25273D]">
+      > */}
+      <div className="flex flex-col gap-[16px] drop-shadow-2xl">
+        <div className="flex h-[48px] items-center rounded-[5px] bg-white px-[20.11px] text-[12px]/[100%] dark:bg-[#25273D] dark:text-[#9495A5]">
+          <div className="mr-[16px] h-[20px] w-[20px] rounded-[50%] border border-[#979797] dark:border-[#393A4B]"></div>
+          <input
+            type="text"
+            placeholder="Create a new todo..."
+            id=""
+            {...register("todoText")}
+            onKeyUp={handleAddTodo}
+            // onChange={handleTodoInput}
+            // value={todoText}
+            className="w-full font-josefin-sans text-[12px]/[100%] outline-none sm:text-[18px]/[100%]"
+          />
+        </div>
+        <div className="divide-y divide-[#C8CBE7] rounded-[5px] bg-white dark:divide-[#393A4B] dark:bg-[#25273D]">
+          <DragDropProvider
+            onDragEnd={async (event) => {
+              if (event.canceled) return;
+
+              const { source } = event.operation;
+
+              if (isSortable(source)) {
+                const { initialIndex, index } = source;
+
+                if (initialIndex !== index) {
+                  const newItems = [...allTodos];
+                  const [removed] = newItems.splice(initialIndex, 1);
+                  newItems.splice(index, 0, removed);
+                  newItems.forEach((item, index) => (item.position = index));
+                  await updateItemPosition();
+                  setAllTodos(newItems);
+                }
+              }
+            }}
+          >
             {allTodos[0] ? (
               todoElements
             ) : (
@@ -285,32 +355,32 @@ function TodoList() {
                 </p>
               </div>
             )}
-
-            <div className="flex h-[48px] items-center rounded-b-[5px] bg-white px-[20.11px] py-[16px] text-[12px]/[100%] dark:bg-[#25273D]">
-              <div className="flex w-full items-center justify-between font-josefin-sans text-[#9495A5] dark:text-[#5B5E7E]">
-                <div className="text-[12px]/[100%] tracking-[-0.25px] sm:text-[14px]">
-                  {allTodos.length} items left
-                </div>
-                <div className="hidden sm:block">
-                  <TabBar list={list} setList={setList} />
-                </div>
-                <button
-                  onClick={clearCompletedHandler}
-                  className="text-[12px]/[100%] tracking-[-0.25px] hover:text-[#494C6B] active:text-[#3A7CFD] sm:text-[14px] dark:hover:text-[#C8CBE7]"
-                >
-                  Clear Completed
-                </button>
+          </DragDropProvider>
+          <div className="flex h-[48px] items-center rounded-b-[5px] bg-white px-[20.11px] py-[16px] text-[12px]/[100%] dark:bg-[#25273D]">
+            <div className="flex w-full items-center justify-between font-josefin-sans text-[#9495A5] dark:text-[#5B5E7E]">
+              <div className="text-[12px]/[100%] tracking-[-0.25px] sm:text-[14px]">
+                {allTodos.length} items left
               </div>
+              <div className="hidden sm:block">
+                <TabBar list={list} setList={setList} />
+              </div>
+              <button
+                onClick={clearCompletedHandler}
+                className="text-[12px]/[100%] tracking-[-0.25px] hover:text-[#494C6B] active:text-[#3A7CFD] sm:text-[14px] dark:hover:text-[#C8CBE7]"
+              >
+                Clear Completed
+              </button>
             </div>
           </div>
-          <div className="sm:hidden">
-            <TabBar list={list} setList={setList} />
-          </div>
         </div>
-        <p className="self-center font-josefin-sans text-[14px]/[100%] tracking-[-0.25px] text-[#9495A5] dark:text-[#5B5E7E]">
-          Drag and drop to reorder list
-        </p>
-      </DragDropProvider>
+        <div className="sm:hidden">
+          <TabBar list={list} setList={setList} />
+        </div>
+      </div>
+      <p className="self-center font-josefin-sans text-[14px]/[100%] tracking-[-0.25px] text-[#9495A5] dark:text-[#5B5E7E]">
+        Drag and drop to reorder list
+      </p>
+      {/* </DragDropProvider> */}
     </>
   );
 }
